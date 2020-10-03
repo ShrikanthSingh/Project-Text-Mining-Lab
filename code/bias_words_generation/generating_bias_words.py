ec83cab6f7eb71cdd11a9e6c33b045493bec51e3
unknown <balaji01@ads.uni-passau.de>
2020-10-03 19:51:23 +0200

    coding sections of the project

---
+++ code/bias_words_generation/generating_bias_words.py
@@ -0,0 +1,64 @@
+'''
+PHASE RESPONSIBILITY - SHRIKANTH SINGH BALAJI SINGH - 87683
+'''
+import itertools
+import multiprocessing
+import random
+
+import numpy as np
+from gensim.models import Word2Vec
+
+word2vec_model = Word2Vec.load('../generate_embeddings/embeddings.model')
+
+
+def generate_combinations(seedwords, n):
+    combinations = list()
+    for i, _tuple in enumerate(itertools.combinations(random.sample(seedwords, len(seedwords)), n)):
+        combinations.append(_tuple)
+        if i > 25000:
+            break
+    return combinations
+
+
+def generate_seed_words(combination):
+    # compute mean of the vectors for n combination of words
+    vectors = list()
+    for i in combination:
+        vectors.append(word2vec_model.wv[i])
+    avg_vector = np.mean(vectors, axis=0)
+
+    # get top 5 similar words for a word using the trained Word2Vec model
+    similar_words = [x[0] for x in word2vec_model.wv.most_similar(positive=[avg_vector], topn=5)]
+
+    return similar_words
+
+
+def bias_word_generation():
+    seedwords = ['radical', 'democrat', 'republican', 'bigot', 'racist']
+
+    for i in range(25):
+        combinations = generate_combinations(list(set(seedwords)), 5)
+
+        pool = multiprocessing.Pool()
+        similar_words = pool.map(generate_seed_words, combinations)
+        pool.close()
+
+        similar_words = [word for sublist in similar_words for word in sublist]
+        seedwords += similar_words
+
+        print('Loop: {}, Bias words length: {}'.format(i, len(set(seedwords))))
+
+    # sort and write the bias words to the text file
+    bias_words = sorted(list(set(seedwords)))
+    with open('bias_words.txt', 'w') as f:
+        for item in bias_words:
+            f.write('{}\n'.format(item))
+    f.close()
+
+
+if __name__ == '__main__':
+    bias_word_generation()
+
+    read_file = open('bias_words.txt', 'r')
+    words = read_file.read().split('\n')
+    print('Total bias words: {}'.format(len(words) - 1))
---
+++ code/preprocessing/preprocessing.py
@@ -0,0 +1,37 @@
+"""
+Phase responsibility: Harshil Jagadishbhai Darji - 87647
+"""
+
+import re
+
+import nltk
+import numpy as np
+import pandas as pd
+from nltk.stem import WordNetLemmatizer
+
+# download wordnet for lemmatizer and create lemmatizer object
+nltk.download('wordnet')
+lmtzr = WordNetLemmatizer()
+
+
+def clean(_text):
+    """
+    This method removes any non-alphanumeric text from the given string and
+    performs Lemmatization before returning the string.
+    """
+    _cleaned = re.sub(r"(@[A-Za-z0-9]+)|(#[A-Za-z0-9]+)|('s)|([^A-Za-z\t])|(http\S+)", ' ', _text).split()
+    cleaned = [lmtzr.lemmatize(w) for w in _cleaned]
+    return (' '.join(cleaned)).strip().lower()
+
+
+data = pd.read_csv('../scrapping/raw_data/raw_data.csv')
+data = data.drop(data.columns[0], axis=1).drop_duplicates(subset='ArtText').dropna().reset_index(drop=True)
+data = data[~data['ArtText'].str.contains('Please enable cookies on your web browser in order to continue.')].reset_index(drop=True)
+data['label'] = np.where((data['Democrat_Vote'] == 'Neutral') & (data['Republican_Vote'] == 'Neutral'), 0, 1)
+data['Text'] = data['ArtTitle'] + ' ' + data['ArtText'] + ' ' + data['ArtSummary']
+
+with open('./data.csv', 'w') as f:
+    f.write('text,label\n')
+    for i, line in enumerate(data['Text']):
+        f.write('{},{}\n'.format(clean(line), data['label'][i]))
+f.close()
